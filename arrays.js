46f2b63db76a96049b41e15a12059950321e7e93
Mendy Goorevitch <menachemgoorevitch@gmail.com>
2017-12-10 17:03:19 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -62,6 +62,6 @@ function addElementToEndOfArray(array, element) {
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-  array.shift(element);
+  array.pop(element);
   return array;
 }
